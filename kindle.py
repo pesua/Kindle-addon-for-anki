72d0430cf658bfb0097bcb9efc2a55704b7899a2
Anton Chernetskiy <anton.chernetskiy@fabricww.com>
2015-03-12 13:01:10 +0200

    Automatically find kindle db

--- kindle.py
+++ kindle.py
@@ -11,6 +11,12 @@ from anki.importing.noteimp import NoteImporter, ForeignNote
 desk = "Test"
 cardType = "Basic (and reversed card)-1e33a"
 
+def findDB():
+    searchDir = "/media"
+    for root, dirs, files in os.walk(searchDir):
+        if ("vocab.db" in files) and root.endswith("system/vocabulary"):
+            return os.path.join(root, "vocab.db")
+
 def importCards():
     did = mw.col.decks.id(desk)
     mw.col.decks.select(did)
@@ -24,34 +30,40 @@ def importCards():
     mw.col.models.save(model)
 
     # import into the collection
-    importer = KindleImporter(mw.col, "")
-    importer.initMapping()
-    importer.run()
+    db = findDB()
+    if db:
+        importer = KindleImporter(mw.col, db)
+        importer.initMapping()
+        importer.run()
 
-    showInfo("Import log: \n" + '\n'.join(importer.log))
+        showInfo("Import log: \n" + '\n'.join(importer.log))
 
-    # display imported cards
-    browser = Browser(mw)
-    browser.form.searchEdit.setEditText("added:1 tag:kindle mid:" + `model['id']`)
-    browser.form.searchEdit.keyPressEvent(QKeyEvent(QEvent.KeyPress, Qt.Key_Enter, Qt.KeyboardModifierMask))
+        # display imported cards
+        browser = Browser(mw)
+        browser.form.searchEdit.setEditText("added:1 tag:kindle mid:" + `model['id']`)
+        browser.form.searchEdit.keyPressEvent(QKeyEvent(QEvent.KeyPress, Qt.Key_Enter, Qt.KeyboardModifierMask))
+    else:
+        showInfo("Can't find kindle")
+
 
 action = QAction("Import new words from Kindle", mw)
 mw.connect(action, SIGNAL("triggered()"), importCards)
 mw.form.menuTools.addAction(action)
 
 class KindleImporter(NoteImporter):
-    imported = 0
     importMode = 1
 
     def __init__(self, *args):
         NoteImporter.__init__(self, *args)
+        self.dbFile = args[1]
+        self.log.append("Importing words from file " + self.dbFile + " to desk " + desk)
 
     def readWords(self):
         con = None
         try:
-            con = lite.connect('/home/acher/Documents/Anki/addons/vocab.db')
+            con = lite.connect(self.dbFile)
             cur = con.cursor()
-            cur.execute('SELECT w.stem, l.usage FROM WORDS as w join LOOKUPS as l on w.id = l.word_key ORDER BY w.timestamp ASC;')
+            cur.execute('SELECT w.stem, l.usage FROM WORDS as w join LOOKUPS as l on w.id = l.word_key where w.category = 0 ORDER BY w.timestamp ASC;')
             rows = cur.fetchall()
             return rows
         except lite.Error, e:
